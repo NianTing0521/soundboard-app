d229d46a607aa26e4e53a8f3e354cbf700a222fc
NianTing0521 <123862127+NianTing0521@users.noreply.github.com>
2026-02-13 12:25:20 +0800

    Update service worker to version 3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'soundboard-v2';
+const CACHE_NAME = 'soundboard-v3';
 const ASSETS = [
   'index.html',
   'style.css',
@@ -25,3 +25,4 @@ self.addEventListener('fetch', (e) => {
   );
 
 });
+
